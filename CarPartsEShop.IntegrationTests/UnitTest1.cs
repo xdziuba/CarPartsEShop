014bcd88a8dcf06be54560109783894426c85062
xdziuba <xdziuba@gmail.com>
2025-06-11 19:18:10 +0200

    Added xUnit test projects

---
+++ CarPartsEShop.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CarPartsEShop.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Do something here
+        }
+    }
+}
